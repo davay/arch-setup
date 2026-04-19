8a445d25972225746ab98df4ebc5c46c33b8d2cb
Devin Lim <davay@devinl.im>
2026-04-18 23:23:24 -0700

    initial stuff, basic init + docker + duckdns

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-sudo pacman -S --needed --noconfirm base-devel git
-git clone https://aur.archlinux.org/paru.git /tmp/paru
-cd /tmp/paru && makepg -si --noconfirm
-
-sudo pacman -S less github-cli ghostty neovim docker zsh ansible
-
-chsh -s /bin/zsh
-
-paru -S 1password zen-browser-bin oh-my-zsh-git
-
-yes | /usr/share/oh-my-zsh/tools/install.sh
+read -rsp "Sudo password: " PASS
+sudo pacman -S --needed --noconfirm ansible
+echo
+(
+    export ANSIBLE_BECOME_PASS="$PASS"
+    ansible-playbook ./playbooks/init.yml
+)
