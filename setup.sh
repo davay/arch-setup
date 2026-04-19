da56adc391a864786f72ede4118894a0bee89dcf
Devin Lim <davay@devinl.im>
2026-04-18 23:31:57 -0700

    fix aur, add requirements.yml

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 read -rsp "Sudo password: " PASS
-echo "$PASS" | sudo -S pacman -S --needed --noconfirm ansible
 echo
+echo "$PASS" | sudo -S --prompt="" pacman -S --needed --noconfirm ansible
+ansible-galaxy collection install -r requirements.yml
 (
   export ANSIBLE_BECOME_PASS="$PASS"
   ansible-playbook ./playbooks/init.yml
