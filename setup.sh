11efa45d8679dbe3f94ed96c1ce79846e5be90f8
Devin Lim <davay@devinl.im>
2026-04-18 23:26:31 -0700

    fix sudo asked twice

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 read -rsp "Sudo password: " PASS
-sudo pacman -S --needed --noconfirm ansible
+echo "$PASS" | sudo -S pacman -S --needed --noconfirm ansible
 echo
 (
-    export ANSIBLE_BECOME_PASS="$PASS"
-    ansible-playbook ./playbooks/init.yml
+  export ANSIBLE_BECOME_PASS="$PASS"
+  ansible-playbook ./playbooks/init.yml
 )
